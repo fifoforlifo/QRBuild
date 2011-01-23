3a0b6c374c5af7f9f0dbcc88fcbe4970a352edd4
Avinash Baliga <fifoforlifo@gmail.com>
2011-01-22 18:44:29 -0600

    Reformatted some code to eagle-claw style.

--- QRBuild/Translations/ToolChain/MsCsc/CSharpCompileExtensions.cs
+++ QRBuild/Translations/ToolChain/MsCsc/CSharpCompileExtensions.cs
@@ -77,78 +77,62 @@ namespace QRBuild.Translations.ToolChain.MsCsc
             StringBuilder b = new StringBuilder();
             b.AppendFormat("/nologo ");
             //-- Input Options
-            foreach (string reference in p.AssemblyReferences)
-            {
+            foreach (string reference in p.AssemblyReferences) {
                 b.AppendFormat("/r:\"{0}\" ", reference);
             }
-            foreach (string module in p.InputModules)
-            {
+            foreach (string module in p.InputModules) {
                 b.AppendFormat("/addmodule:\"{0}\" ", module);
             }
             //-- Output Options
-            if (!String.IsNullOrEmpty(p.OutputFilePath))
-            {
+            if (!String.IsNullOrEmpty(p.OutputFilePath)) {
                 b.AppendFormat("/out:\"{0}\" ", p.OutputFilePath);
             }
-            if (!String.IsNullOrEmpty(p.TargetFormat))
-            {
+            if (!String.IsNullOrEmpty(p.TargetFormat)) {
                 b.AppendFormat("/target:{0} ", p.TargetFormat);
             }
             //-- Code Generation
-            if (p.Debug)
-            {
+            if (p.Debug) {
                 b.AppendFormat("/debug ");
             }
-            if (p.Optimize)
-            {
+            if (p.Optimize) {
                 b.AppendFormat("/optimize ");
             }
             //-- Errors and Warnings
-            if (p.WarnAsError) 
-            {
+            if (p.WarnAsError) {
                 b.AppendFormat("/warnaserror ");
             }
             b.AppendFormat("/warn:{0} ", p.WarnLevel);
             //-- Language
-            if (p.Checked)
-            {
+            if (p.Checked) {
                 b.AppendFormat("/checked ");
             }
-            if (p.Unsafe)
-            {
+            if (p.Unsafe) {
                 b.AppendFormat("/unsafe ");
             }
-            foreach (string define in p.Defines)
-            {
+            foreach (string define in p.Defines) {
                 b.AppendFormat("/define:{0} ", define);
             }
-            if (!String.IsNullOrEmpty(p.LanguageVersion))
-            {
+            if (!String.IsNullOrEmpty(p.LanguageVersion)) {
                 b.AppendFormat("/langversion:{0} ", p.LanguageVersion);
             }
             //-- Miscellaneous
             // NOTE: p.NoConfig may not be supplied in a response file
             //-- Advanced
-            if (!String.IsNullOrEmpty(p.MainType))
-            {
+            if (!String.IsNullOrEmpty(p.MainType)) {
                 b.AppendFormat("/main:{0} ", p.MainType);
             }
-            if (p.FullPaths) 
-            {
+            if (p.FullPaths) {
                 b.AppendFormat("/fullpaths ");
             }
-            if (!String.IsNullOrEmpty(p.PdbFilePath)) 
-            {
+            if (!String.IsNullOrEmpty(p.PdbFilePath)) {
                 b.AppendFormat("/pdb:\"{0}\" ", p.PdbFilePath);
             }
-            if (!String.IsNullOrEmpty(p.ModuleAssemblyName)) 
-            {
+            if (!String.IsNullOrEmpty(p.ModuleAssemblyName)) {
                 b.AppendFormat("/moduleassemblyname:{0} ", p.ModuleAssemblyName);
             }
 
             //  ExtraArgs goes at the end.
-            if (!String.IsNullOrEmpty(p.ExtraArgs)) 
-            {
+            if (!String.IsNullOrEmpty(p.ExtraArgs)) {
                 b.AppendFormat("{0} ", p.ExtraArgs);
             }
 
